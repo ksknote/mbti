aeaf8216fea93bd1f4e9381fb6eae509835ab59b
bible <mongry999@gmail.com>
2023-05-23 17:44:53 +0900

    [modified] 결과페이지 주석 추가

--- src/components/resultpage.js
+++ src/components/resultpage.js
@@ -1,22 +1,22 @@
 import { useNavigate } from "react-router-dom";
 
 function Resultpage(props) {
-  let data = {
-    ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-    ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-    ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-    ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-    ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-    ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-    ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-    ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-    ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-  };
+  // let data = {
+  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+  // };
 
   let navigate = useNavigate();
   return (
     <div>
-      <div>{data[props.MBTI].title};</div>
+      {/* <div>{data[props.MBTI].title};</div> */}
       <button
         onClick={() => {
           navigate("/");
