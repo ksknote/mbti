eea08ad72e43ba4ed8aebc6c2838a36118332e7c
bible <mongry999@gmail.com>
2023-05-24 22:27:30 +0900

    [add]결과페이지 mbti 가져오기

--- src/components/resultpage.js
+++ src/components/resultpage.js
@@ -1,22 +1,29 @@
 import { useNavigate } from "react-router-dom";
 
 function Resultpage(props) {
-  // let data = {
-  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-  //   ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
-  // };
+  let data = {
+    ENTP: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    ENTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    ENFP: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    ENFJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    ESTP: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    ESTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    ESFP: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    ESFJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    INTP: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    INTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    INFP: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    INFJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    ISTP: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    ISTJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    ISFP: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+    ISFJ: { title: "짱쏀ESTJ", cont: "ESTJ는 아주 강합니다." },
+  };
 
   let navigate = useNavigate();
   return (
     <div>
-      {/* <div>{data[props.MBTI].title};</div> */}
+      <div>{data[props.MBTI].title};</div>
       <button
         onClick={() => {
           navigate("/");
